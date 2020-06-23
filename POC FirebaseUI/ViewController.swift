6e47124cb82022698d089e38d0f9d65b0003f2a1
Christopher San Diego <csandiego@gmail.com>
2020-06-23 19:13:42 +0800

    Initial Commit

---
+++ POC FirebaseUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC FirebaseUI
+//
+//  Created by Christopher San Diego on 6/23/20.
+//  Copyright © 2020 Christopher San Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
